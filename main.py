48553135227b5cc6f83fc81fa42877085e160b86
scollorec <132073589+scollorec@users.noreply.github.com>
2023-05-20 20:04:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print("ICI")
     test.test()
 
     #my_json = slack = os.environ['GOOGLE_API_KEY']
